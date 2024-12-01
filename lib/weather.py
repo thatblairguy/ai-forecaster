dd2e5c506f97e91f686ad67042d8935b09112860
Blair <ThatBlairGuy@users.noreply.github.com>
2024-11-30 20:41:59 -0500

    Add retries to weather report retrieval.

--- lib/weather.py
+++ lib/weather.py
@@ -1,34 +1,49 @@
 import aiohttp
-import pynws
+from pynws import SimpleNWS, call_with_retry
+from typing import Tuple
 
 
 class Weather:
+    """
+    Fetch weather reports from the US National Weather Service using pynws.
+
+    :param userid: A string containing the user id (email address) for  authentication.
+    """
 
     def __init__(self, userid: str) -> None:
+
+        if not isinstance(userid, str) or not userid.strip():
+            raise ValueError("User ID must be a non-empty string.")
+
         self.userid = userid
         self.session = aiohttp.ClientSession()
 
     async def close(self):
         await self.session.close()  # Clean up the session
 
-    async def get_forecast(self, location: tuple) -> str:
+    async def get_forecast(self, location: Tuple[float, float]) -> str:
+        """
+        Get the weather forecast for a specific location.
+
+        :param location: A tuple (latitude, longitude) representing the location.
+
+        :return: A detailed forecast string.
+        """
 
         if not isinstance(location, tuple) or len(location) != 2:
             raise ValueError(
                 "Location must be a tuple with two elements: (latitude, longitude)."
             )
 
-        nws = pynws.SimpleNWS(*location, self.userid, self.session)
+        retry_interval = 0.0
+        max_retry = 10.0
+
+        nws = SimpleNWS(*location, self.userid, self.session)
         await nws.set_station()
-        await nws.update_observation()
-        await nws.update_forecast()
-        await nws.update_alerts_forecast_zone()
-        await nws.update_detailed_forecast()
-        # print(nws.observation)
-        # print(nws.forecast[0])
-        # print(nws.alerts_forecast_zone)
-        # pprint(nws.detailed_forecast.get_details_for_time(date))
-        # pprint(date)
+        await call_with_retry(nws.update_observation, retry_interval,  max_retry)
+        await call_with_retry(nws.update_forecast, retry_interval,  max_retry)
+        await call_with_retry(nws.update_alerts_forecast_zone, retry_interval,  max_retry)
+        await call_with_retry(nws.update_detailed_forecast, retry_interval,  max_retry)
 
         text = nws.forecast[0]["detailedForecast"]
 
